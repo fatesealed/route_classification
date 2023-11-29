e37ddee5e1bf9a9321de9f35dbf144a82e8b2457
fatesealed <940268050@qq.com>
2023-11-30 00:07:24 +0800

    修改transformer 不用直接相加了 只用第一个做全连接

--- models/Transformer.py
+++ models/Transformer.py
@@ -1,7 +1,4 @@
-import numpy as np
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class ModelConfig(object):
@@ -14,146 +11,33 @@ class ModelConfig(object):
 
         self.dropout = 0.5  # 随机失活
         self.learning_rate = 5e-4  # 学习率
-        self.dim_model = 1
-        self.hidden = 1024
-        self.last_hidden = 512
-        self.num_head = 1
-        self.num_encoder = 4
+        # self.dim_model = 1
+        # self.hidden = 1024
+        # self.last_hidden = 512
+        self.num_head = 10
+        self.num_encoder = 6
 
 
 '''Attention Is All You Need'''
 
 
 class Model(nn.Module):
-    def __init__(self, config):
+    def __init__(self, model_config, data_config):
         super(Model, self).__init__()
-        if config.embedding_pretrained is not None:
-            self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
-        else:
-            self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab - 1)
-
-        # self.encoder_layer = nn.TransformerEncoderLayer(d_model=100)
-        # self.postion_embedding = Positional_Encoding(config.embed, config.pad_size, config.dropout, config.device)
-        # self.encoder = Encoder(config.dim_model, config.num_head, config.hidden, config.dropout)
-        # self.encoders = nn.ModuleList([
-        #     copy.deepcopy(self.encoder)
-        #     for _ in range(config.num_encoder)])
-        #
-        # self.fc1 = nn.Linear(config.pad_size * config.dim_model, config.num_classes)
+        self.embedding = nn.Embedding.from_pretrained(
+            data_config.embedding_pretrained,
+            freeze=False) if data_config.embedding_pretrained is not None else nn.Embedding(data_config.n_vocab,
+                                                                                            data_config.embed,
+                                                                                            padding_idx=data_config.n_vocab - 1)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=data_config.embed, nhead=model_config.num_head,
+                                                        batch_first=True)
+        self.encoders = nn.TransformerEncoder(self.encoder_layer, num_layers=model_config.num_encoder)
+        self.fc = nn.Linear(data_config.embed, data_config.num_classes)
 
     def forward(self, x):
-        out = self.embedding(x[0])
-        out = self.postion_embedding(out)
-        for encoder in self.encoders:
-            out = encoder(out)
-        out = out.view(out.size(0), -1)
-        # out = torch.mean(out, 1)
-        out = self.fc1(out)
-        return out
-
-
-class Encoder(nn.Module):
-    def __init__(self, dim_model, num_head, hidden, dropout):
-        super(Encoder, self).__init__()
-        self.attention = Multi_Head_Attention(dim_model, num_head, dropout)
-        self.feed_forward = Position_wise_Feed_Forward(dim_model, hidden, dropout)
-
-    def forward(self, x):
-        out = self.attention(x)
-        out = self.feed_forward(out)
-        return out
-
-
-class Positional_Encoding(nn.Module):
-    def __init__(self, embed, pad_size, dropout, device):
-        super(Positional_Encoding, self).__init__()
-        self.device = device
-        self.pe = torch.tensor(
-            [[pos / (10000.0 ** (i // 2 * 2.0 / embed)) for i in range(embed)] for pos in range(pad_size)])
-        self.pe[:, 0::2] = np.sin(self.pe[:, 0::2])
-        self.pe[:, 1::2] = np.cos(self.pe[:, 1::2])
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        out = x + nn.Parameter(self.pe, requires_grad=False).to(self.device)
-        out = self.dropout(out)
-        return out
-
-
-class Scaled_Dot_Product_Attention(nn.Module):
-    '''Scaled Dot-Product Attention '''
-
-    def __init__(self):
-        super(Scaled_Dot_Product_Attention, self).__init__()
-
-    def forward(self, Q, K, V, scale=None):
-        '''
-        Args:
-            Q: [batch_size, len_Q, dim_Q]
-            K: [batch_size, len_K, dim_K]
-            V: [batch_size, len_V, dim_V]
-            scale: 缩放因子 论文为根号dim_K
-        Return:
-            self-attention后的张量，以及attention张量
-        '''
-        attention = torch.matmul(Q, K.permute(0, 2, 1))
-        if scale:
-            attention = attention * scale
-        # if mask:  # TODO change this
-        #     attention = attention.masked_fill_(mask == 0, -1e9)
-        attention = F.softmax(attention, dim=-1)
-        context = torch.matmul(attention, V)
-        return context
-
-
-class Multi_Head_Attention(nn.Module):
-    def __init__(self, dim_model, num_head, dropout=0.0):
-        super(Multi_Head_Attention, self).__init__()
-        self.num_head = num_head
-        assert dim_model % num_head == 0
-        self.dim_head = dim_model // self.num_head
-        self.fc_Q = nn.Linear(dim_model, num_head * self.dim_head)
-        self.fc_K = nn.Linear(dim_model, num_head * self.dim_head)
-        self.fc_V = nn.Linear(dim_model, num_head * self.dim_head)
-        self.attention = Scaled_Dot_Product_Attention()
-        self.fc = nn.Linear(num_head * self.dim_head, dim_model)
-        self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(dim_model)
-
-    def forward(self, x):
-        batch_size = x.size(0)
-        Q = self.fc_Q(x)
-        K = self.fc_K(x)
-        V = self.fc_V(x)
-        Q = Q.view(batch_size * self.num_head, -1, self.dim_head)
-        K = K.view(batch_size * self.num_head, -1, self.dim_head)
-        V = V.view(batch_size * self.num_head, -1, self.dim_head)
-        # if mask:  # TODO
-        #     mask = mask.repeat(self.num_head, 1, 1)  # TODO change this
-        scale = K.size(-1) ** -0.5  # 缩放因子
-        context = self.attention(Q, K, V, scale)
-
-        context = context.view(batch_size, -1, self.dim_head * self.num_head)
-        out = self.fc(context)
-        out = self.dropout(out)
-        out = out + x  # 残差连接
-        out = self.layer_norm(out)
-        return out
-
-
-class Position_wise_Feed_Forward(nn.Module):
-    def __init__(self, dim_model, hidden, dropout=0.0):
-        super(Position_wise_Feed_Forward, self).__init__()
-        self.fc1 = nn.Linear(dim_model, hidden)
-        self.fc2 = nn.Linear(hidden, dim_model)
-        self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(dim_model)
-
-    def forward(self, x):
-        out = self.fc1(x)
-        out = F.relu(out)
-        out = self.fc2(out)
-        out = self.dropout(out)
-        out = out + x  # 残差连接
-        out = self.layer_norm(out)
+        out = self.embedding(x)
+        out = self.encoders(out)
+        # out = out.reshape(out.size(0), -1)
+        out = out[:, 0, :]
+        out = self.fc(out)
         return out
